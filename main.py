023667d07617e41261caae1388d8547cbf4c0798
P4blo003 <pablogonzalezgarcia003@gmail.com>
2025-01-25 11:41:57 +0100

    Función listar GPUs CUDA.

---
+++ main.py
@@ -0,0 +1,8 @@
+# Modules
+from pym2ai import List_GPUs
+# -------
+
+
+if __name__ == "__main__":
+
+    List_GPUs('tensorflow')
---
+++ pym2ai/__init__.py
@@ -0,0 +1,3 @@
+# Modules
+from pym2ai.devices import List_GPUs
+# -------
