cca101c8ba1f90abcc317d4bdb94d88cc6c9135d
P4blo003 <Pablogonzalezgarcia003@gmail.com>
2025-04-22 14:49:13 +0000

    Conversación básica con IA LLM (DeepSeek 8B)

--- source/config/settings.py
+++ source/config/settings.py
@@ -2,7 +2,9 @@
 from dotenv import load_dotenv
 import os
 
+from typing import List
+
 # ---- Propiedades ---- #
 load_dotenv()
 
-MODEL_PATH:str = os.getenv("MODEL_PATH")
+MODEL_CACHE_PATH:str = os.path.join(os.getcwd(), os.getenv("MODEL_CACHE_PATH"))    # Carga el directorio de cache.
